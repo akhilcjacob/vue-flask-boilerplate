6a4c748f20ddf61923a06204eef590d4783a08fc
akhilcjacob <akhilcjacob@gmail.com>
2018-02-11 16:23:09 -0500

    Finished the front end

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="row pt-lg-3">
-    <div class="col-lg-12">
-      <h1 class="">Template Header 1</h1>
-      <p class="subtitle">
-        Akhil Jacob
-      </p>
-    </div>
+  <div>
+    <h3>Random number from backend: {{ randomNumber }}</h3>
+    <button @click="getRandom" type="button" class="btn btn-primmary-outline">Get a new number</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Hello'
+  data () {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandom () {
+    // this.randomNumber = this.getRandomInt(1, 100)
+      this.randomNumber = this.getRandomFromBackend()
+    },
+    getRandomFromBackend () {
+      const path = `http://localhost:5000/api/random`
+      axios.get(path)
+        .then(response => {
+          this.randomNumber = response.data.randomNumber
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 }
-</script>
 
-<style scoped="">
-p.subtitle {
-  font-size: 2rem;
-  color: #999999;
-}
-</style>
+</script>
